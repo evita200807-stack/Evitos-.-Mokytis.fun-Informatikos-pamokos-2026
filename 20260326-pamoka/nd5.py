d1c646bcf719dfd8c48cc34a837a070a04035181
evita200807-stack <evita200807@gmail.com>
2026-04-01 10:48:46 -0700

    Update nd5.py

--- 20260326-pamoka/nd5.py
+++ 20260326-pamoka/nd5.py
@@ -10,3 +10,11 @@ a + b > c
 a + c > b
 b + c > a
 """
+
+a = int(input("Įvesk kraštinę a: ")) 
+b = int(input("Įvesk kraštinę b: ")) 
+c = int(input("Įvesk kraštinę c: "))
+a + b > c and a + c > b and b + c > a: 
+ print("Galima sudaryti trikampį") 
+else:
+     print("Negalima sudaryti trikampio")
