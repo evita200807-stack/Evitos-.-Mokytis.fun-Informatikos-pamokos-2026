cc5785d036ed423432e3134ffbe1b31975d5e6e3
evita200807-stack <evita200807@gmail.com>
2026-04-01 10:19:05 -0700

    Update nd1.py

--- 20260326-pamoka/nd1.py
+++ 20260326-pamoka/nd1.py
@@ -3,3 +3,8 @@ Slaptažodžio sistema
 jei įveda "admin123" -> "Prisijungta"
 kitaip -> "Klaida"
 """
+slaptažodis = input('įveskite slaptažodį')
+if slaptažodis == 'admin123'
+print('prisijungta')
+else:
+  print('klaida')
